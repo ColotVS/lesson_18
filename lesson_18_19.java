653f0200731d43674eb3751b62b185cad98a1729
ColotVS <kolotilov98@mail.ru>
2024-03-07 16:44:20 +0700

    Работа с интерфейсами Урок 18, задача 19

---
+++ lesson_18_19.java
@@ -0,0 +1,42 @@
+public class lesson_18_19 {
+    //Создай два интерфейса — Swimmable и Flyable.
+    //В будущем их можно будет использовать для обозначения классов, объекты которых умеют плавать или летать.
+}
+interface Flyable {
+
+}
+
+interface Swimmable {
+
+}
+
+    //Имплементируй интерфейс Eatable только для классов, объекты которых могут быть съедобными, из этого списка:
+    //Apple — яблоко
+    //Carrot — морковь
+    //Fire — огонь
+    //Fish — рыба
+    //Stone — камень
+
+interface Eatable {
+
+}
+
+class Fire {
+
+}
+
+class Stone {
+
+}
+
+class Carrot implements Eatable{
+
+}
+
+class Fish implements Eatable{
+
+}
+
+class Apple implements Eatable{
+
+}
