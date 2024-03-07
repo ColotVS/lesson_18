849bc0affd0e3e888629e84a806909ec999456c5
ColotVS <kolotilov98@mail.ru>
2024-03-07 17:07:14 +0700

    Суть интерфейсов, часть 2 Урок 18, задача 24

---
+++ lesson_18_24.java
@@ -0,0 +1,17 @@
+public class lesson_18_24 {
+    //Существуют 2 интерфейса — Swimmable и Flyable, указывающие на то, что объект умеет плавать и/или летать соответственно.
+    //Расставь эти интерфейсы для классов Zeppelin (Дирижабль),
+    //Ship (Корабль) и JamesBondCar (Машина Джеймса Бонда) в соответствии с их возможностями.
+}
+
+class Zeppelin implements Flyable{
+
+}
+
+class Ship implements Swimmable{
+
+}
+
+class JamesBondCar implements Flyable,Swimmable{
+
+}
